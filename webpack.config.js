4a8fe37ac23fb21478fa66987c010b28667eab87
minmein <minmein86@gmail.com>
2024-11-15 16:06:01 +0800

    Try to deploy to vercel again

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,8 @@ const discountCodeExample = require('./modules/discount-code/webpack.config');
 const splitExample = require('./modules/discount-redemption-split/webpack.config');
 
 module.exports = function(env, argv) {
-    // Modify the configs to ensure they output to public directory
-    const discountConfig = discountCodeExample(env, argv);
-    const splitConfig = splitExample(env, argv);
-
-    // Update output paths
-    discountConfig.output = {
-        ...discountConfig.output,
-        path: path.resolve(__dirname, 'public/discount-code')
-    };
-
-    splitConfig.output = {
-        ...splitConfig.output,
-        path: path.resolve(__dirname, 'public/split')
-    };
-
-    return [discountConfig, splitConfig];
+    return [
+        discountCodeExample(env, argv),
+        splitExample(env, argv),
+    ];
 };
