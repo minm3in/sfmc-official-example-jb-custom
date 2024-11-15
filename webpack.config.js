1082f71884629d7a877a9af9da8bc4660a00bf46
minmein <minmein86@gmail.com>
2024-11-15 14:45:08 +0800

    to deploy to vercel for output file location

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,20 @@ const discountCodeExample = require('./modules/discount-code/webpack.config');
 const splitExample = require('./modules/discount-redemption-split/webpack.config');
 
 module.exports = function(env, argv) {
-    return [
-        discountCodeExample(env, argv),
-        splitExample(env, argv),
-    ];
+    // Modify the configs to ensure they output to public directory
+    const discountConfig = discountCodeExample(env, argv);
+    const splitConfig = splitExample(env, argv);
+
+    // Update output paths
+    discountConfig.output = {
+        ...discountConfig.output,
+        path: path.resolve(__dirname, 'public/discount-code')
+    };
+
+    splitConfig.output = {
+        ...splitConfig.output,
+        path: path.resolve(__dirname, 'public/split')
+    };
+
+    return [discountConfig, splitConfig];
 };
